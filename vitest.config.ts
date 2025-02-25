ff69f1c18a9704a60dec1884f68e4c427c32b745
Cassio Zen <cassiozen@gmail.com>
2025-02-24 23:48:55 -0600

    Cleanup

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,6 +4,6 @@ export default defineConfig({
   test: {
     globals: true,
     environment: "jsdom",
-    setupFiles: ["./setupTest.ts"], // point to your setup file
+    setupFiles: ["./setupTest.ts"],
   },
 });
