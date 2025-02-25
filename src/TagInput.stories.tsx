b3e01aad3594745d143872540c8090a0103f6188
Cassio Zen <cassiozen@gmail.com>
2025-02-24 23:46:14 -0600

    Improved stories

--- src/TagInput.stories.tsx
+++ src/TagInput.stories.tsx
@@ -1,4 +1,30 @@
+import { action } from "@ladle/react";
 import TagInput from "./TagInput";
-import "./index.css";
+import "./main.css";
 
-export const Basic = () => <TagInput />;
+const meta = {
+  title: "TagInput",
+};
+
+export default meta;
+
+// Basic usage
+export const Basic = () => {
+  return (
+    <div>
+      <TagInput onTagsChange={action("TagsChange")} className="input-medium" />
+    </div>
+  );
+};
+
+// With some initial tags
+export const WithInitialTags = () => {
+  return (
+    <TagInput
+      initialTags={["JavaScript", "TypeScript"]}
+      onTagsChange={action("TagsChange")}
+      placeholder="Add tags..."
+      className="input-medium"
+    />
+  );
+};
